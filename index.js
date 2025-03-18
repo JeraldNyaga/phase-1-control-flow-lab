8a7018c2b0af8b74c40e39a3581bdd2f57692cd8
judgeOstrich <jeraldnyaga15@gmail.com>
2025-03-19 01:14:09 +0300

    Control-flow statements

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetVal){
+  feetVal = parseInt(feetVal, 10);
+  if(feetVal <= 400){
+    return `This one is on me!`
+  }
+  else if(feetVal > 400 && feetVal <= 2000){
+    return `That will be twenty bucks.`
+  } else if(feetVal > 2000 && feetVal <= 2500){
+    return `I will gladly take your thirty bucks.`
+  } else{
+    return `No can do.`
+  }
+}
+function ternaryCheckCity(cityName){
+  if(cityName === 'NYC'){
+    return `Ok, sounds good.`
+  }
+  else{
+    return `No go.`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return `Thank you so much.`
+      break;
+    case 'not as generous':
+      return `Thank you.`
+      break;
+    default:
+      return `Bye.`
+  }
 }
